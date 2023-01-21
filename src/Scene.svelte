71763badc70af8ce04017df6d908a73dda59382a
Lacy Morrow <me@lacymorrow.com>
2023-01-20 21:42:31 -0500

    fucked

---
+++ src/Scene.svelte
@@ -0,0 +1,63 @@
+<!-- Scene.svelte -->
+<template>
+  <div class="scene">
+    <img class="background" src="background.png" alt="16-bit scene">
+    <img class="cat" src="cat.png" alt="Cat using a cell phone" on:click={zoomIn}>
+    <div class="cell-phone" style={cellPhoneStyle}>
+      <Video videoUrl={videoUrl} />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Video from './Video.svelte';
+  export let videoUrl;
+  let zoomed = false;
+  let cellPhoneStyle = { display: "none" };
+
+  function zoomIn() {
+    if (!zoomed) {
+      zoomed = true;
+      cellPhoneStyle = { display: "block" };
+    } else {
+      zoomed = false;
+      cellPhoneStyle = { display: "none" };
+    }
+  }
+</script>
+
+<style>
+  .scene {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .background {
+    width: 100%;
+    height: 100%;
+  }
+
+  .cat {
+    position: absolute;
+    bottom: 10%;
+    right: 10%;
+    width: 20%;
+    cursor: pointer;
+  }
+
+  .cell-phone {
+    position: absolute;
+    bottom: 10%;
+    right: 10%;
+    width: 50%;
+    height: 50%;
+    overflow: hidden;
+    transition: all 0.5s ease-in-out;
+  }
+
+  .cell-phone .video {
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ src/components/splash.svelte
@@ -0,0 +1,51 @@
+<script>
+  let catState = 'idle';
+
+  setInterval(() => {
+    switch (catState) {
+      case 'idle':
+        catState = 'playing';
+        break;
+      case 'playing':
+        catState = 'stretching';
+        break;
+      case 'stretching':
+        catState = 'idle';
+        break;
+    }
+
+    document.querySelectorAll('.8-bit-cat-animation img').forEach(img => {
+      img.style.display = 'none';
+    });
+    document.querySelector(`.8-bit-cat-animation .${catState}`).style.display = 'block';
+  }, 3000);
+</script>
+
+<style>
+  .retro-tv {
+    position: relative;
+    width: 600px;
+    height: 400px;
+    background-image: url('retro-tv-background.png');
+    background-size: cover;
+  }
+
+  .8-bit-cat-animation {
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    width: 100px;
+    height: 100px;
+ 
+
+  }
+</style>
+
+<div class="retro-tv">
+  <iframe src="https://www.youtube.com/embed/your_video_id" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="8-bit-cat-animation">
+    <img src="8-bit-cat-idle.png" alt="8-bit cat idle" class="idle" />
+    <img src="8-bit-cat-playing.png" alt="8-bit cat playing" class="playing" style="display: none;" />
+    <img src="8-bit-cat-stretching.png" alt="8-bit cat stretching" class="stretching" style="display: none;" />
+  </div>
+</div>
